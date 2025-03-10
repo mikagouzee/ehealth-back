12082a93c2459442a71d0ee7dd884c5d11b29d62
mika <michel.gouzee@hotmail.com>
2025-03-10 14:19:47 +0100

    add spotbugs

--- src/main/java/com/example/demo/controller/AppointmentController.java
+++ src/main/java/com/example/demo/controller/AppointmentController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
